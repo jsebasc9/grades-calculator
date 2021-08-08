37f87297a994c98d09cc249a8a7024b25a7a041e
Juan Sebastian Calderon <juans.jc2@hotmail.com>
2021-08-08 20:01:01 +0800

    Code commented

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ index = 0
 num_assessments = int(input('Please type number of assessments: '))
 
 while(True):
+    #Checking if the number of assessments is greater than 1, also, if the lists are not full
     if index < num_assessments and num_assessments >= 1:  
         #Loop that repeats num_assessments times to fill a list with the names of assessments
         while index < num_assessments:
             
             input_user = input('Type name of assessment\n')
+            #Checking if the assessment name is in the list
             if (input_user in list_names):
                 print('The assessment is already in the list, please put a different name')
                 break
@@ -21,9 +23,9 @@ while(True):
             
         print(list_names)
         print(list_values)
-        
+    #If the list are full the program ends    
     elif index != 0 and index == num_assessments:
         break
-
+    #If the user input less than 1 assessments the program asks for the number again.
     else:    
         num_assessments = int(input('Error: you have to put at least 1, please type number of assessments: '))
