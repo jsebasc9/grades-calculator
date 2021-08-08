699a7a7dfa1823a0e3af78b7b0a33189c5ba89e6
Juan Sebastian Calderon <juans.jc2@hotmail.com>
2021-08-08 19:57:07 +0800

    Flowchart and code to step 3

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ list_values = []
 index = 0
 num_assessments = int(input('Please type number of assessments: '))
 
-#Loop that repeats num_assessments times to fill a list with the names of assessments
-print('Please type names of assessments')
-while index < num_assessments:
-    inputuser = input()
-    list_names.append(inputuser)
-    index += 1
-print(list_names)
-index = 0
+while(True):
+    if index < num_assessments and num_assessments >= 1:  
+        #Loop that repeats num_assessments times to fill a list with the names of assessments
+        while index < num_assessments:
+            
+            input_user = input('Type name of assessment\n')
+            if (input_user in list_names):
+                print('The assessment is already in the list, please put a different name')
+                break
+            else:    
+                list_names.append(input_user)
+                input_user = int(input('Please put value of assessment\n'))
+                list_values.append(input_user)
+                index += 1
+            
+        print(list_names)
+        print(list_values)
+        
+    elif index != 0 and index == num_assessments:
+        break
 
-#Loop that repeats num_assessments times to fill a list with the values of assessments
-print('Please values of assessments')
-while index < num_assessments:
-    input_user = int(input())
-    list_values.append(input_user)
-    index += 1
-print(list_values)
-index = 0
+    else:    
+        num_assessments = int(input('Error: you have to put at least 1, please type number of assessments: '))
