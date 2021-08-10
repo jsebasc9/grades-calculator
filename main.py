9d1489c46b5097465402a04b29771ca988bc5100
Juan Sebastian Calderon <juans.jc2@hotmail.com>
2021-08-10 23:23:12 +0800

    Step 6 Completed

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 #Initialization of variables and lists
 list_names = []
 list_values = []
-list_total_marks = []
+list_total_marks_students = []
+list_total_marks_class = []
 num_students = 0
+top_class_mark = 0
 
 #Function to calculate Grades
 def calculate_grade(mark):
@@ -23,8 +25,9 @@ def calculate_grade(mark):
 
 num_assessments = int(input('Please type number of assessments: '))
 
-for assessment in range(num_assessments):       
-    input_user = input('Type name of assessment : ')   
+for assessment in range(num_assessments):
+    print('Type name of assessment ', assessment, end=' : ')
+    input_user = input()   
     list_names.append(input_user)
     input_user = int(input('Please put value of assessment : '))
     list_values.append(input_user)
@@ -35,14 +38,15 @@ if total_sum_values != 100:
     print('Assessments dont sum up 100')
 else:
     #Restarting Index in the lists
-    num_students = int(input('Type number of students : '))
+    num_students = int(input('\nType number of students : '))
             #Starting students stage and calculation of grades
 
     for student in range(num_students):
-        name_student = input('Type name of student : ')
+        print('\nType name of student', student, end=' : ')
+        name_student = input()
 
         for value in range(len(list_values)):
-            print('What did ', name_student, 'get out of', list_values[value], ' in the ',list_names[value], '? : ')
+            print('What did ', name_student, 'get out of', list_values[value], ' in the ',list_names[value], '? ', end=' : ')
             mark = int(input())
             #If the user enters a mark superior thank the assessment, it will change the mark to the maximum of the assessment.
             if mark > list_values[value]:
@@ -51,12 +55,23 @@ else:
             if mark < 0:
                 mark = 0
             mark_converted = mark*100/list_values[value]
-            list_total_marks.append(mark_converted)
+            list_total_marks_students.append(mark_converted)
             grade = calculate_grade(mark_converted)
             print(mark, ' out of' , list_values[value], 'is' , grade )
             value += 1
-        total_mark = sum(list_total_marks)/len(list_total_marks)
+        total_mark = sum(list_total_marks_students)/len(list_total_marks_students)
         grade = calculate_grade(total_mark)
         print(name_student, ' has a total mark of' , int(total_mark), '(' , grade, ')' )
-        list_total_marks.clear()
+        list_total_marks_class.append(total_mark)
+        if total_mark > top_class_mark:
+            top_student_name = name_student
+            top_class_mark = total_mark
+            
+        list_total_marks_students.clear()
+
+    print('\nAll Marks entered !')
+    class_mark = sum(list_total_marks_class)/len(list_total_marks_class)
+    grade = calculate_grade(class_mark)
+    print('The class average is' , grade )
+    print('The top student is ', top_student_name, 'with a total mark of ', int(top_class_mark))
 
